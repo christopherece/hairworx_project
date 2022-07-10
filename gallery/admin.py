1752433f72c607eacb9c0e1b9158b981a611dc4c
christopherece <christopheranchetaece@gmail.com>
2022-07-10 21:41:17 +1200

    Edit Admin2

--- gallery/admin.py
+++ gallery/admin.py
@@ -3,7 +3,16 @@ from django.contrib import admin
 # Register your models here.
 from .models import Gallery
 
-admin.site.register(Gallery)
+
+class GalleryAdmin(admin.ModelAdmin):
+    list_display = (
+        'photo',
+    )
+    search_fields = ('photo', 'created_date')
+    list_per_page = 25
+
+
+admin.site.register(Gallery, GalleryAdmin)
 
 
 
